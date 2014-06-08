baa5bfb6e6a0000dc1466c813a914b192161dc0d
Nan Monnand Deng <monnand@gmail.com>
2014-06-08 09:08:28 -0400

    we don't need exlet

--- pulet/testpul/main.go
+++ pulet/testpul/main.go
@@ -1,13 +1,17 @@
 package main
 
 import (
+	"flag"
 	"log"
 
 	"github.com/vmarmol/vertigo/pulet"
 )
 
+var argContainerId = flag.String("id", "13ccf48b7cec704c1cc441e87a4c66102a1ead1fa3dbcec6ac255c53e802b631", "container id")
+var argCommand = flag.String("cmd", "/bin/true", "command")
+
 func main() {
-	id := "e0fdd7e222041ec15e41d596708f384d6084ef598f6aee709c7423b38b5f30fb"
+	id := *argContainerId
 	pul := pulet.NewPulet()
 	importSpec := &pulet.ImportSpec{
 		SourceHost: "107.178.222.32",
@@ -18,10 +22,18 @@ func main() {
 	if err != nil {
 		log.Fatal(err)
 	}
-	err = pul.RunImage(img, nil, []string{
-		"sleep",
-		"2m",
-	})
+	/*
+		err = pul.RunImage(img, []string{
+			"-d",
+			"-p",
+			"3306:3306",
+			"-e",
+			"MYSQL_PASS=\"mypass\"",
+		}, []string{
+			"/run.sh",
+		})
+	*/
+	err = pul.RunImage(img, nil, []string{"/bin/true"})
 	if err != nil {
 		log.Fatal(err)
 	}
